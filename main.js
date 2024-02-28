dc13a37f02c1eeab64f721b41c6db569589019aa
Rel <olellervin@gmail.com>
2024-02-28 13:57:06 -0800

    Upoloading V1.0 of the website to git

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Link Highlighting
+const linkHighlight = function(self) {
+  self.style.backgroundColor = 'white'
+}
+
+const unHighlight = function(self) {
+  self.style.backgroundColor = '#FF6C22'
+}
