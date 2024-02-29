d90b288948ee82f920b39de475ecc092ea177d08
Rel <olellervin@gmail.com>
2024-02-28 16:36:41 -0800

    Update contact info in footer

--- docs/main.js
+++ docs/main.js
@@ -7,3 +7,19 @@ const linkHighlight = function(self) {
 const unHighlight = function(self) {
   self.style.backgroundColor = '#FF6C22'
 }
+
+
+
+// Copy Email
+
+function copyEmail() {
+  // Get the text field
+  var copyText = document.getElementById("myEmail");
+
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText('olellervin@gmail.com');
+
+  // Alert the copied text
+  alert(" Email copied! Looking forward to hearing from you!");
+}
